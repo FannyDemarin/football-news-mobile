295db29615b2231a579ce3e9c0d49d76bcc87dc4
FannyDemarin <fademarin@gmail.com>
2025-11-20 04:50:57 +0700

    test keystore

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,28 +1,7 @@
-import java.util.Properties
-import java.io.FileInputStream
-import java.util.Base64
-
-// Charger les propriétés du keystore
-val keystoreProperties = Properties()
-val keystorePropertiesFile = rootProject.file("key.properties")
-if (keystorePropertiesFile.exists()) {
-    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
-}
-
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // Le plugin Flutter doit être appliqué après Android et Kotlin
     id("dev.flutter.flutter-gradle-plugin")
-}
-
-val keystoreBase64 = System.getenv("KEY_JKS")
-val keystorePassword = System.getenv("KEY_PASSWORD")
-
-val keystoreFile = File("release.keystore")
-
-if (!keystoreFile.exists()) {
-    keystoreFile.writeBytes(Base64.getDecoder().decode(keystoreBase64))
 }
 
 android {
@@ -49,10 +28,12 @@ android {
 
     signingConfigs {
         create("release") {
-            keyAlias =  "upload"
-            keyPassword = keystorePassword
-            storeFile = keystoreFile
-            storePassword = keystorePassword 
+            val password = System.getenv("KEY_PASSWORD")
+
+            storeFile = file("release-keystore.jks")
+            storePassword = password
+            keyPassword = password
+            keyAlias = "upload"
         }
     }
 
